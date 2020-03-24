2aa633d6adbda8e7e165571fae5e41b9665ddb03
Braulio-Eugenio <61022835+Braulio-Eugenio@users.noreply.github.com>
2020-03-24 16:54:35 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/estrelas/financas/repository/AvaliacaoRepository.java
@@ -0,0 +1,10 @@
+package br.com.zup.estrelas.financas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.zup.estrelas.financas.entity.Avaliacao;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+
+    
+    
+}
