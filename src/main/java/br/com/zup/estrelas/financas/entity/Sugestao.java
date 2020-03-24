dd56567dcdf6538d382c2098388f086b8bd26141
klasyfyd01 <kobokoe@gmail.com>
2020-03-24 17:24:36 -0300

    Sugestao

--- src/main/java/br/com/zup/estrelas/financas/entity/Sugestao.java
+++ src/main/java/br/com/zup/estrelas/financas/entity/Sugestao.java
@@ -9,9 +9,10 @@ import javax.persistence.Id;
 @Entity
 public class Sugestao {
 
+
     @Column(name = "id_sugestao")
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idSugestao;
 
     @Column
@@ -50,10 +51,6 @@ public class Sugestao {
 
     public TipoSugestao getTipoSugestao() {
         return tipoSugestao;
-    }
-
-    public void setTipoSugestao(TipoSugestao tipoSugestao) {
-        this.tipoSugestao = tipoSugestao;
     }
 
 
