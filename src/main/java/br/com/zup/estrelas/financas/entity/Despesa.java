5d57475e94b514bf540c57dbd7713cac95e826bf
fabio-elioterio <fabioelioterio.77@gmail.com>
2020-03-24 17:27:37 -0300

    Atualizando DespesaRepository

--- src/main/java/br/com/zup/estrelas/financas/entity/Despesa.java
+++ src/main/java/br/com/zup/estrelas/financas/entity/Despesa.java
@@ -1,5 +1,59 @@
 package br.com.zup.estrelas.financas.entity;
 
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Despesa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long idDespesa;
+    
+    @Column(nullable = false)
+    private float valor;
+    
+   @Column(nullable = false, name = "tipo_de_despesa")
+   private String tipoDeDespesa; 
+    
+    @Column(nullable = false)
+    private LocalDate vencimento;
+
+    //TODO: CRIAR ASSOCIAÇÕES
+    
+    public Long getIdDespesa() {
+        return idDespesa;
+    }
+
+    public void setIdDespesa(Long idDespesa) {
+        this.idDespesa = idDespesa;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getTipoDeDespesa() {
+        return tipoDeDespesa;
+    }
+
+    public void setTipoDeDespesa(String tipoDeDespesa) {
+        this.tipoDeDespesa = tipoDeDespesa;
+    }
+
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(LocalDate vencimento) {
+        this.vencimento = vencimento;
+    }
 }
--- src/main/java/br/com/zup/estrelas/financas/repository/DespesaRepository.java
+++ src/main/java/br/com/zup/estrelas/financas/repository/DespesaRepository.java
@@ -1,5 +1,10 @@
 package br.com.zup.estrelas.financas.repository;
 
-public interface DespesaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.zup.estrelas.financas.entity.Despesa;
+
+@Repository
+public interface DespesaRepository extends JpaRepository<Despesa, Long> {
 
 }
