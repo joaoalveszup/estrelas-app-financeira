b0f550bcd41df6bb169f59ef43b7d51eecf29e9d
Braulio-Eugenio <61022835+Braulio-Eugenio@users.noreply.github.com>
2020-03-25 00:21:12 -0300

    Ataluzação - Criação CRUD

---
+++ src/main/java/br/com/zup/estrelas/financas/controller/AvaliacaoController.java
@@ -0,0 +1,56 @@
+package br.com.zup.estrelas.financas.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.estrelas.financas.entity.Avaliacao;
+import br.com.zup.estrelas.financas.repository.AvaliacaoRepository;
+
+@RestController
+@RequestMapping("/avaliacao")
+public class AvaliacaoController {
+    @Autowired
+    AvaliacaoRepository repository;
+
+    @PostMapping
+    public Avaliacao insereAvaliacao(@RequestBody Avaliacao avaliacao) {
+        return this.repository.save(avaliacao);
+
+    }
+
+    @GetMapping(path = "/{id_avaliacao}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Avaliacao buscaAvaliacao(@PathVariable Long idAvaliacao) {
+        return this.repository.findById(idAvaliacao).get();
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Avaliacao> buscaAvaliacoes() {
+        return (List<Avaliacao>) repository.findAll();
+
+    }
+
+    @DeleteMapping(path = "/{id_avaliacao}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void deletaAvaliacao(@PathVariable Long idAvaliacao) {
+        this.repository.deleteById(idAvaliacao);
+    }
+
+    @PutMapping(path = "/{id_avaliacao}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Avaliacao alteraAvaliacao(@PathVariable Long idAvaliacao,
+            @RequestBody Avaliacao avaliacao) {
+        Avaliacao avaliacaoBanco = repository.findById(idAvaliacao).get();
+        avaliacao.setComentario(avaliacao.getComentario());
+        avaliacao.setIdUsuario(avaliacao.getIdUsuario());
+        avaliacao.setNotaAvaliacao(avaliacao.getNotaAvaliacao());
+        return this.repository.save(avaliacaoBanco);
+
+    }
+
+}
