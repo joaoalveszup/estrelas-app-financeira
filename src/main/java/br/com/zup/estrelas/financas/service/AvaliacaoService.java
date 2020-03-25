07fd670d5400a1f7df09fe3e68ddb4e627fb69ce
Braulio-Eugenio <61022835+Braulio-Eugenio@users.noreply.github.com>
2020-03-25 18:40:52 -0300

    Criação do service

--- src/main/java/br/com/zup/estrelas/financas/service/AvaliacaoService.java
+++ src/main/java/br/com/zup/estrelas/financas/service/AvaliacaoService.java
@@ -1,5 +1,41 @@
 package br.com.zup.estrelas.financas.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.com.zup.estrelas.financas.entity.Avaliacao;
+import br.com.zup.estrelas.financas.repository.AvaliacaoRepository;
+
+@Service
 public class AvaliacaoService {
 
+    @Autowired
+    AvaliacaoRepository repository;
+
+
+    public Avaliacao insereAvaliacao(Avaliacao avaliacao) {
+
+        return repository.save(avaliacao);
+
+    }
+
+
+    public Avaliacao buscaAvaliacao() {
+        return null;
+    }
+
+
+    public List<Avaliacao> buscaAvaliacoes() {
+        return (List<Avaliacao>) repository.findAll();
+
+    }
+
+    public void deletaAvaliacao(Long idAvaliacao) {
+        this.repository.deleteById(idAvaliacao);
+    }
+
+    public Avaliacao alteraAvaliacao() {
+        return null;
+
+    }
 }
