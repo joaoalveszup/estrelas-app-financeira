b580688504d80e38c88019b5c10d8097b7e73123
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2020-03-25 13:19:10 -0300

    Atualizando...

--- src/main/java/br/com/zup/estrelas/financas/entity/Dependente.java
+++ src/main/java/br/com/zup/estrelas/financas/entity/Dependente.java
@@ -5,24 +5,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Dependente {
-
+    
     @Id
     @Column(name = "id_dependente", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDependente;
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-
+    
     @Column(nullable = false)
     private String nome;
-
+    
     @Column(nullable = false)
     private String parentesco;
-
+    
     @Column(nullable = false)
     private Float renda;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario", insertable=false, updatable=false)
+    @JsonIgnore
+    private Usuario usuario;
 
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+    
     public Long getIdDependente() {
         return idDependente;
     }
@@ -55,5 +66,20 @@ public class Dependente {
         this.renda = renda;
     }
 
+    public Usuario getUsuario() {
+        return usuario;
+    }
 
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
 }
