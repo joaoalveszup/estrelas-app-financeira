68cd8da557ca65d0c787b3bc6d61fa96c492ffd9
fabio-elioterio <fabioelioterio.77@gmail.com>
2020-03-25 14:23:13 -0300

    Atualizando Repositorio

--- src/main/java/br/com/zup/estrelas/financas/repository/DespesaRepository.java
+++ src/main/java/br/com/zup/estrelas/financas/repository/DespesaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.zup.estrelas.financas.entity.DespesaEntity;
 
+
 @Repository
 public interface DespesaRepository extends JpaRepository<DespesaEntity, Long> {
 
