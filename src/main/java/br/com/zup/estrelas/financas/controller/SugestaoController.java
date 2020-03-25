36543898606e0ec7f7be31f666b2710b8374994c
klasyfyd01 <kobokoe@gmail.com>
2020-03-25 11:40:38 -0300

    Sugestao Atualizado

--- src/main/java/br/com/zup/estrelas/financas/controller/SugestaoController.java
+++ src/main/java/br/com/zup/estrelas/financas/controller/SugestaoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.zup.estrelas.financas.entity.Sugestao;
+<<<<<<< HEAD
 import br.com.zup.estrelas.financas.repository.SugestaoService;
 
 @RequestMapping("/sugestoes")
@@ -26,10 +27,29 @@ public class SugestaoController {
     @GetMapping(path = "/{idSugestao}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Iterable<Sugestao> buscaSugestao(@PathVariable Sugestao sugestao) {
         return services.findAll();
+=======
+import br.com.zup.estrelas.financas.repository.SugestaoRepository;
+
+@RequestMapping("/sugestao")
+public class SugestaoController {
+
+    @Autowired
+    SugestaoRepository repository;
+
+    @PostMapping
+    public Sugestao insereSugestao(@RequestBody Sugestao sugestao) {
+        return this.repository.save(sugestao);
+    }
+
+    @GetMapping(path = "/{sugestao}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Iterable<Sugestao> buscaSugestao(@PathVariable Sugestao sugestao) {
+        return repository.findAll();
+>>>>>>> Sugestao Atualizado
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Sugestao> buscaSugestao() {
+<<<<<<< HEAD
         return (List<Sugestao>) services.findAll();
 
     }
@@ -44,3 +64,15 @@ public class SugestaoController {
 }
 
 
+=======
+        return (List<Sugestao>) repository.findAll();
+
+    }
+
+    @DeleteMapping("/sugestao")
+    public void delete(@PathVariable Long Sugestao) {
+        repository.delete(null);
+    }
+    
+}
+>>>>>>> Sugestao Atualizado
