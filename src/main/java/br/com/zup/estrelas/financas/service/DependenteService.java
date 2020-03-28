10dfcf36ed4a76680e632abb564e8bf2f654944f
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2020-03-28 17:38:52 -0300

    Organizando imports

--- src/main/java/br/com/zup/estrelas/financas/service/DependenteService.java
+++ src/main/java/br/com/zup/estrelas/financas/service/DependenteService.java
@@ -1,7 +1,6 @@
 package br.com.zup.estrelas.financas.service;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.zup.estrelas.financas.entity.Dependente;
