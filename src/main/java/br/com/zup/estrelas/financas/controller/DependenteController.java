f781542b19e71e5616484ef31e59c333b1c82eda
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2020-03-30 22:40:23 -0300

    Refatorando DependenteController

--- src/main/java/br/com/zup/estrelas/financas/controller/DependenteController.java
+++ src/main/java/br/com/zup/estrelas/financas/controller/DependenteController.java
@@ -15,35 +15,35 @@ import br.com.zup.estrelas.financas.entity.Dependente;
 import br.com.zup.estrelas.financas.service.DependenteService;
 
 @RestController
-@RequestMapping (value = {"/usuarios" , "{idUsuario}" , "/dependentes" , ""})
+@RequestMapping ("/usuarios/{idUsuario}/dependentes")
 public class DependenteController {
 
     @Autowired
     DependenteService dependenteService;
 
-    @PostMapping(path = "/usuarios/{idUsuario}/dependentes")
+    @PostMapping
     public Dependente insereDependente(@RequestBody Dependente dependente) {
         return this.dependenteService.insereDependente(dependente);
     }
 
-    @GetMapping(path = "/usuarios/{idUsuario}/dependentes/{idDependente}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(path = "{idDependente}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Dependente buscaDependente(@PathVariable Long idDependente) {
         return this.dependenteService.buscaDependente(idDependente);
     }
 
-    @GetMapping(path = "/usuarios/{idUsuario}/dependentes", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Dependente> buscaDependentes() {
         return this.dependenteService.buscaDependentes();
     }
 
-    @PutMapping(path = "/usuarios/{idUsuario}/dependentes/{idDependente}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @PutMapping(path = "{idDependente}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Dependente modificaDependente(@PathVariable Long idDependente,
             @RequestBody Dependente dependente) {
 
         return this.dependenteService.modificaDependente(idDependente, dependente);
     }
 
-    @DeleteMapping(path = "/usuarios/{idUsuario}/dependentes/{idDependente}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @DeleteMapping(path = "{idDependente}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public void deletaDependente(@PathVariable Long idDependente) {
         this.dependenteService.deletaDependente(idDependente);
     }
