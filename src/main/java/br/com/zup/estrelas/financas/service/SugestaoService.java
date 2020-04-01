5b599c4813b7a0efd7a586de41b8256b0fde41f3
klasyfyd01 <kobokoe@gmail.com>
2020-04-01 00:59:19 -0300

    Enum and Regras completed

--- src/main/java/br/com/zup/estrelas/financas/service/SugestaoService.java
+++ src/main/java/br/com/zup/estrelas/financas/service/SugestaoService.java
@@ -13,6 +13,11 @@ public class SugestaoService {
     SugestaoRepository sugestaoRepository;
 
     public Sugestao insereSugestao(Sugestao sugestao) {
+        if (limitaCaratereTitulo(sugestao.getTitulo())
+                || limitaCaratereDescricao(sugestao.getDescricao())) {
+            return null;
+        }
+
         return this.sugestaoRepository.save(sugestao);
     }
 
@@ -30,9 +35,39 @@ public class SugestaoService {
 
     public Sugestao alteraSugestao(Long idSugestao, Sugestao sugestao) {
         Sugestao sugestaoBanco = sugestaoRepository.findById(idSugestao).get();
-        sugestaoBanco.setTitulo(sugestao.getTitulo());
-        sugestaoBanco.setDescricao(sugestao.getDescricao());
-        sugestaoBanco.setTipoSugestao(sugestao.getTipoSugestao());
+
+        if (limitaCaratereDescricao(sugestao.getDescricao())
+                || limitaCaratereTitulo(sugestao.getTitulo())) {
+            return null;
+        }
+        if (validaTituloEDescricao(sugestao.getTitulo(), sugestao.getDescricao())) {
+            sugestaoBanco.setDescricao(sugestao.getDescricao());
+            sugestaoBanco.setTitulo(sugestao.getTitulo());
+        }
         return this.sugestaoRepository.save(sugestaoBanco);
     }
+
+    public boolean validaTituloEDescricao(String titulo, String descricao) {
+        if (titulo.equals(titulo) || descricao.equals(descricao)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean limitaCaratereTitulo(String titulo) {
+
+        if (titulo.length() > 40) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean limitaCaratereDescricao(String descricao) {
+        if (descricao.length() > 400) {
+            return true;
+        }
+        return false;
+    }
+
 }
+
