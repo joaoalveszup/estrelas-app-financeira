4cc3252fb0563772332c987454f4fed9b1ae2bfe
joaoalveszup <joao.alves@zup.com.br>
2020-04-07 17:18:15 -0300

    Adiciona ao pacote correto de exception

--- src/main/java/br/com/zup/estrelas/financas/exceptions/ValidaCampoECaratereException.java
+++ src/main/java/br/com/zup/estrelas/financas/exceptions/ValidaCampoECaratereException.java
@@ -1,4 +1,4 @@
-package br.com.zup.estrelas.financas.exception;
+package br.com.zup.estrelas.financas.exceptions;
 
 public class ValidaCampoECaratereException extends Exception {
     public ValidaCampoECaratereException (String mensagem) {
