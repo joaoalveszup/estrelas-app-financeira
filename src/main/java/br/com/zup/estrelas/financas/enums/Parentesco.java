cf92b8d1d059d229cb289f78aac922020f2ff91e
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2020-04-08 13:01:57 -0300

    Revisão - Dependentes

--- src/main/java/br/com/zup/estrelas/financas/enums/Parentesco.java
+++ src/main/java/br/com/zup/estrelas/financas/enums/Parentesco.java
@@ -1,28 +1,23 @@
 package br.com.zup.estrelas.financas.enums;
 
 public enum Parentesco {
-    
-    FILHO("filho"),
-    CONJUGE("cônjuge"),
-    PAI("pai"),
-    MAE("mãe"),
-    OUTRO("outro");
-    
+
+    FILHO("filho"), CONJUGE("cônjuge"), PAI("pai"), MAE("mãe"), OUTRO("outro");
+
     private String value;
- 
-    Parentesco(String value) {
+
+    private Parentesco(String value) {
         this.value = value;
     }
- 
+
     public String getValue() {
         return value;
     }
-    
+
     @Override
     public String toString() {
         return this.value;
     }
 }
-
 
 
