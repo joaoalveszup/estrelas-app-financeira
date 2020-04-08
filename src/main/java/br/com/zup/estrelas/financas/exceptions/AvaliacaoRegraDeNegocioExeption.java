7b1545f88b1b792d3feba1e2b097ef1eb8ca8eb5
Braulio-Eugenio <61022835+Braulio-Eugenio@users.noreply.github.com>
2020-04-08 11:39:04 -0300

    Refatoração de revisão, foi trocado nomes de variaveis para melhor compreensão e feita uma revisão para possiveis imports desnecessarios, e possiveis códigos repetidos.

--- src/main/java/br/com/zup/estrelas/financas/exceptions/AvaliacaoRegraDeNegocioExeption.java
+++ src/main/java/br/com/zup/estrelas/financas/exceptions/AvaliacaoRegraDeNegocioExeption.java
@@ -1,5 +1,6 @@
 package br.com.zup.estrelas.financas.exceptions;
 
+@SuppressWarnings("serial")
 public class AvaliacaoRegraDeNegocioExeption extends Exception {
 
     public AvaliacaoRegraDeNegocioExeption(String menssagem) {
