a3d5166936db4a3704e21a2909b368a30c66252a
joaoalveszup <joao.alves@zup.com.br>
2020-04-16 13:46:18 -0300

    Remove import desnecessário.

--- src/main/java/br/com/zup/estrelas/financas/service/InvestimentoService.java
+++ src/main/java/br/com/zup/estrelas/financas/service/InvestimentoService.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.NumberUtils;
 
 @Service
 public class InvestimentoService {
