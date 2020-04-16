fbedfe4631f7aeeec31035447ccce11fdfed82be
joaoalveszup <joao.alves@zup.com.br>
2020-04-16 13:16:10 -0300

    Adiciona endpoint e queries necessárias para retornar o valor gasto com objetivos no mes corrente.

--- src/main/java/br/com/zup/estrelas/financas/repository/InvestimentoRepository.java
+++ src/main/java/br/com/zup/estrelas/financas/repository/InvestimentoRepository.java
@@ -1,9 +1,11 @@
 package br.com.zup.estrelas.financas.repository;
 
+import br.com.zup.estrelas.financas.entity.Investimento;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import br.com.zup.estrelas.financas.entity.Investimento;
 
 @Repository
 public interface InvestimentoRepository extends JpaRepository<Investimento, Long> {
@@ -13,6 +15,10 @@ public interface InvestimentoRepository extends JpaRepository<Investimento, Long
     List<Investimento> findByPagoAndIdObjetivo(Boolean Pago, Long idObjetivo);
 
     Investimento findByIdInvestimentoAndIdObjetivo(Long idInvestimento, Long idObjetivo);
-    
+
     List<Investimento> findAllByIdObjetivo(Long idObjetivo);
+
+    @Query(value = "select SUM(i.valor) from Investimento i where i.idObjetivo in :objetivos and i.dataVencimento between :dataInicio and :dataFim")
+    Double sumInvestimentos(List<Long> objetivos, LocalDate dataInicio, LocalDate dataFim);
+
 }
--- src/main/java/br/com/zup/estrelas/financas/repository/ObjetivoRepository.java
+++ src/main/java/br/com/zup/estrelas/financas/repository/ObjetivoRepository.java
@@ -3,6 +3,7 @@ package br.com.zup.estrelas.financas.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import br.com.zup.estrelas.financas.entity.Objetivo;
 
@@ -12,4 +13,7 @@ public interface ObjetivoRepository extends JpaRepository<Objetivo, Long> {
     List<Objetivo> findAllByIdUsuario(Long idUsuario);
 
     Optional<Objetivo> findByIdUsuarioAndIdObjetivo(Long idUsuario, Long idObjetivo);
+
+    @Query("select o.idObjetivo from Objetivo o where o.idUsuario = :idUsuario")
+    List<Long> findAllIdsFromUser(Long idUsuario);
 }
