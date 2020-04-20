881103e14fbb041f897a48b4cd45778349a8aae9
fabio-elioterio <fabioelioterio.77@gmail.com>
2020-04-20 01:08:38 -0300

    Altera status de crição de entidade para 201 CREATED

--- src/main/java/br/com/zup/estrelas/financas/controller/DespesaController.java
+++ src/main/java/br/com/zup/estrelas/financas/controller/DespesaController.java
@@ -3,7 +3,9 @@ package br.com.zup.estrelas.financas.controller;
 
 import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.zup.estrelas.financas.dto.AtualizaDespesaDto;
 import br.com.zup.estrelas.financas.dto.CriaDespesaDTO;
@@ -27,9 +30,10 @@ public class DespesaController {
 
     @Autowired
     DespesaService despesaService;
-
+    
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "/usuarios/{idUsuario}/despesas")
-    public List<DespesaDTO> insereDespesa(@RequestBody CriaDespesaDTO criaDespesaDto,
+    public List<DespesaDTO> insereDespesa(@Valid @RequestBody CriaDespesaDTO criaDespesaDto,
             @PathVariable Long idUsuario) throws DespesaOuUsuarioNullException {
         return this.despesaService.insereDespesa(criaDespesaDto, idUsuario);
     }
